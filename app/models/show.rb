306d0ec6e9c582b02620a15011056f2e0dd3562b
Fiona Weaver <fiona.l.weaver@gmail.com>
2020-12-17 14:54:53 -0700

    commit tree 6532ec06e4df996230fdffdba4acd43a33a10b38

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,15 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters 
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+     self.actors.collect do |actor|
+      "#{actor.full_name}"
+     end 
+  end 
+
+  def build_network
+    self.character = Character.find_or_create_by("#{self.character.name}")
+  end 
 end
